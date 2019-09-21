ca87bfc407f53e5c0f580f2f3153315eddb5ebeb
Chris Park <cpwebdeveloper99@gmail.com>
2019-09-21 12:26:37 -0700

    finished what-we-do responsive

--- src/Components/StudioDropdownList/StudioDropdownList.js
+++ src/Components/StudioDropdownList/StudioDropdownList.js
@@ -6,16 +6,30 @@ export default function StudioDropdownList(props) {
   return (
     <ul className="page-dropdown-list page-dropdown-list-desktop">
       <li>
-        <NavLink to="/what-we-do">THE STUDIO</NavLink>
+        <NavLink
+          to="/what-we-do"
+          className={props.location.pathname === '/what-we-do' && 'page-active'}
+        >
+          THE STUDIO
+        </NavLink>
       </li>
       <li>
-        <NavLink to="/lessons">LESSONS</NavLink>
+        <NavLink to="/lessons" className={props.location.pathname === '/lessons' && 'page-active'}>
+          LESSONS
+        </NavLink>
       </li>
       <li>
-        <NavLink to="/new-students">PROSPECTIVE STUDENTS</NavLink>
+        <NavLink
+          to="/new-students"
+          className={props.location.pathname === '/new-students' && 'page-active'}
+        >
+          PROSPECTIVE STUDENTS
+        </NavLink>
       </li>
       <li>
-        <NavLink to="/suzuki">SUZUKI METHOD</NavLink>
+        <NavLink to="/suzuki" className={props.location.pathname === '/suzuki' && 'page-active'}>
+          SUZUKI METHOD
+        </NavLink>
       </li>
     </ul>
   );
