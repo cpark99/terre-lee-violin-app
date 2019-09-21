c3ebeb4a2ba34deaf1a017922e6922fe84247004
Chris Park <cpwebdeveloper99@gmail.com>
2019-09-21 04:01:36 -0700

    updating responsive layout-tablet, refactored studio list

--- src/Components/StudioDropdown/StudioDropdown.js
+++ src/Components/StudioDropdown/StudioDropdown.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { NavLink } from 'react-router-dom';
+import StudioDropdownList from '../StudioDropdownList/StudioDropdownList';
 import './StudioDropdown.css';
 
 export default function StudioDropdown(props) {
@@ -19,30 +19,7 @@ export default function StudioDropdown(props) {
       <span className="page-dropdown-button bold" onClick={toggleDropdownMenu}>
         {showDropdown ? <span>-</span> : '+'}
       </span>
-      {showDropdown && (
-        <ul className="page-dropdown-list">
-          {props.location.pathname !== '/what-we-do' && (
-            <li>
-              <NavLink to="/what-we-do">THE STUDIO</NavLink>
-            </li>
-          )}
-          {props.location.pathname !== '/lessons' && (
-            <li>
-              <NavLink to="/lessons">LESSONS</NavLink>
-            </li>
-          )}
-          {props.location.pathname !== '/new-students' && (
-            <li>
-              <NavLink to="/new-students">PROSPECTIVE STUDENTS</NavLink>
-            </li>
-          )}
-          {props.location.pathname !== '/suzuki' && (
-            <li>
-              <NavLink to="/suzuki">SUZUKI METHOD</NavLink>
-            </li>
-          )}
-        </ul>
-      )}
+      {showDropdown && <StudioDropdownList location={props.location} />}
     </div>
   );
 }
--- src/Components/WhatWeDo/WhatWeDo.js
+++ src/Components/WhatWeDo/WhatWeDo.js
@@ -5,6 +5,8 @@ import studentTwo from '../../img/EB2019-SpringRecital-8775.jpg';
 import studentThree from '../../img/EB2019-SpringRecital-8716.jpg';
 import './WhatWeDo.css';
 import StudioDropdown from '../StudioDropdown/StudioDropdown';
+import { Breakpoint } from 'react-socks';
+import StudioDropdownList from '../StudioDropdownList/StudioDropdownList';
 
 export default function WhatWeDo(props) {
   useEffect(() => {
@@ -14,41 +16,53 @@ export default function WhatWeDo(props) {
   return (
     <section id="what-we-do" className="container">
       <ScrollToTopOnMount />
-      <StudioDropdown location={props.location} />
-      <h1 className="page-title">THE VIOLIN STUDIO</h1>
-      <p>
-        At Terre's Suzuki violin studio, students as young as 2 engage in a playful, whole brain
-        activity that creates a strong musical foundation & trains the body to play with ease.
-      </p>
-      <p>Together, we celebrate</p>
-      <ul id="what-we-celebrate-list">
-        <li>
-          <p>Individualized learning</p>
-        </li>
-        <li>
-          <p>Parent/family involvement</p>
-        </li>
-        <li>
-          <p>Group learning with peers</p>
-        </li>
-      </ul>
-      <p>
-        The child is immersed in an environment of positivity, encouragement, and learns to play
-        with technical ease and beautiful tone.
-      </p>
-      <p>
-        The violin studio is centrally located in a residential neighborhood in Santa Cruz,
-        California.
-      </p>
-      <span className="blockquote-quote">"</span>
-      <blockquote>“Beautiful tone, beautiful heart.” - Dr. Suzuki</blockquote>
-      <img className="main-image" src={studentOne} alt="student learing to play violin" />
-      <img className="main-image" src={studentTwo} alt="student playing violin" />
-      <img
-        className="main-image"
-        src={studentThree}
-        alt="Terre Lee teaching student how to play a violin"
-      />
+      <Breakpoint large down>
+        <StudioDropdown location={props.location} />
+      </Breakpoint>
+      <Breakpoint tabletLandscape up>
+        <div id="studio-menu-desktop">
+          <h2>TEACHING STUDIO</h2>
+          <StudioDropdownList location={props.location} />
+        </div>
+      </Breakpoint>
+      <div id="studio-information">
+        <h1 className="page-title">THE VIOLIN STUDIO</h1>
+        <p>
+          At Terre's Suzuki violin studio, students as young as 2 engage in a playful, whole brain
+          activity that creates a strong musical foundation & trains the body to play with ease.
+        </p>
+        <p>Together, we celebrate</p>
+        <ul id="what-we-celebrate-list">
+          <li>
+            <p>Individualized learning</p>
+          </li>
+          <li>
+            <p>Parent/family involvement</p>
+          </li>
+          <li>
+            <p>Group learning with peers</p>
+          </li>
+        </ul>
+        <p>
+          The child is immersed in an environment of positivity, encouragement, and learns to play
+          with technical ease and beautiful tone.
+        </p>
+        <p>
+          The violin studio is centrally located in a residential neighborhood in Santa Cruz,
+          California.
+        </p>
+      </div>
+      <div id="studio-images" className="flex-column-center">
+        <span className="blockquote-quote">"</span>
+        <blockquote>“Beautiful tone, beautiful heart.” - Dr. Suzuki</blockquote>
+        <img className="main-image" src={studentOne} alt="student learing to play violin" />
+        <img className="main-image" src={studentTwo} alt="student playing violin" />
+        <img
+          className="main-image"
+          src={studentThree}
+          alt="Terre Lee teaching student how to play a violin"
+        />
+      </div>
     </section>
   );
 }
