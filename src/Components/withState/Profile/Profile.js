c3a34ed5816bc503d752305944fdc4e5883f27c0
Chris Park <cpwebdeveloper99@gmail.com>
2019-10-01 11:11:35 -0700

    updated rechedule link

--- src/Components/withState/Profile/Profile.js
+++ src/Components/withState/Profile/Profile.js
@@ -56,7 +56,7 @@ export default class Profile extends Component {
             <div id="profile-reschedule" className="flex-column-center">
               <h2 className="profile-section-header">RESCHEDULE LESSON</h2>
               <a
-                href="https://calendly.com/tleeviolin/15min"
+                href="https://calendly.com/tleeviolin/reschedule"
                 target="_blank"
                 rel="noopener noreferrer"
               >
